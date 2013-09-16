2551c821584afc1df769b97a2c47585fd7de1002
Volodymyr Shevchenko <vovic89@gmail.com>
2013-09-16 18:38:18 +0300

    Add: singletoneBuilder tests

---
+++ CRDI Tests/CRDiSingletoneBuilderSpec.m
@@ -0,0 +1,32 @@
+#import <Kiwi.h>
+#import "CRDISingletoneBuilder.h"
+#import "CRDISampleClass.h"
+
+SPEC_BEGIN(CRDiSingletoneBuilderSpec)
+
+describe(@"CRDiSingletoneBuilderSpec", ^{
+    
+    Class cls = [CRDISampleClass class];
+    __block CRDISingletoneBuilder *singletoneBuilder = nil;
+    
+    
+    beforeAll(^{
+        singletoneBuilder = [[CRDISingletoneBuilder alloc] initWithClass:cls];
+    });
+    
+    it(@"Should return always same instance", ^{
+        id instance = [singletoneBuilder build];
+        for (NSInteger i=0; i<10; i++) {
+            id otherInstance = [singletoneBuilder build];
+            [[instance should] equal:otherInstance];
+        }
+    });
+    
+    it(@"Should return instance with same class of instance builder", ^{
+        id instance = [singletoneBuilder build];
+        [[instance should] beKindOfClass:cls];
+    });
+    
+});
+
+SPEC_END
---
+++ CRDI/Code/DIBuilders/DISingletoneBuilder/CRDISingletoneBuilder.m
@@ -0,0 +1,27 @@
+//
+//  CRDISingletoneBuilder.m
+//  CRDI
+//
+//  Created by Vladimir Shevchenko on 16.09.13.
+//  Copyright (c) 2013 CriolloKit. All rights reserved.
+//
+
+#import "CRDISingletoneBuilder.h"
+
+@implementation CRDISingletoneBuilder
+
+- (id)initWithClass:(Class)aClass
+{
+    self = [super init];
+    
+    
+    
+    return self;
+}
+
+- (id)build
+{
+    return nil;
+}
+
+@end
