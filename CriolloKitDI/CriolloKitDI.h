cfd8dc19d8f9fb0773f6739fd1d7dc07d9e0d977
Max Sysenko <max.s@techery.io>
2016-07-26 15:52:53 +0300

    Add public headers to global header

--- CriolloKitDI/CriolloKitDI.h
+++ CriolloKitDI/CriolloKitDI.h
@@ -14,6 +14,25 @@ FOUNDATION_EXPORT double CriolloKitDIVersionNumber;
 //! Project version string for CriolloKitDI.
 FOUNDATION_EXPORT const unsigned char CriolloKitDIVersionString[];
 
-// In this header, you should import all the public headers of your framework using statements like #import <CriolloKitDI/PublicHeader.h>
+#import <CriolloKitDI/CRDIAutoInjector.h>
+#import <CriolloKitDI/CRDIBlockBuilder.h>
+#import <CriolloKitDI/CRDIClassBuilder.h>
+#import <CriolloKitDI/CRDIClassInspector.h>
+#import <CriolloKitDI/CRDIConfiguration.h>
+#import <CriolloKitDI/CRDIContainer.h>
+#import <CriolloKitDI/CRDIDefaultPropertyNameMatcher.h>
+#import <CriolloKitDI/CRDIDependencyBuilder.h>
+#import <CriolloKitDI/CRDIException.h>
+#import <CriolloKitDI/CRDIGlobalAutoInjectionStorage.h>
+#import <CriolloKitDI/CRDIInjector.h>
+#import <CriolloKitDI/CRDIInstanceInjector.h>
+#import <CriolloKitDI/CRDIPropertyNameMatcher.h>
+#import <CriolloKitDI/CRDISingletoneBuilder.h>
+#import <CriolloKitDI/CRDITypedefs.h>
+#import <CriolloKitDI/CriolloKitDI.h>
+#import <CriolloKitDI/DIClassTemplate.h>
+#import <CriolloKitDI/DIPropertyModel.h>
+#import <CriolloKitDI/NSObject+AutoInject.h>
+
 
 
