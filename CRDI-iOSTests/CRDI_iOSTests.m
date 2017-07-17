5dde5d138822a3b5701a8be126a5ae10f47a254b
Eugene Tulusha <tulusheg@gmail.com>
2017-07-17 18:00:36 +0300

    Setup dynamic framework

---
+++ CRDI-iOSTests/CRDI_iOSTests.m
@@ -0,0 +1,39 @@
+//
+//  CRDI_iOSTests.m
+//  CRDI-iOSTests
+//
+//  Created by Eugene on 7/17/17.
+//  Copyright © 2017 CriolloKit. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface CRDI_iOSTests : XCTestCase
+
+@end
+
+@implementation CRDI_iOSTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
