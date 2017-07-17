9bb93144f754ea6570becbc3ab7f00f246a22eca
Eugene Tulusha <tulusheg@gmail.com>
2017-07-17 18:04:47 +0300

    Add umbrella header

---
+++ CRDI/CRDI.h
@@ -0,0 +1,21 @@
+//
+//  CRDI.h
+//  CRDI
+//
+//  Created by Eugene on 7/17/17.
+//  Copyright © 2017 CriolloKit. All rights reserved.
+//
+
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CRDI.
+FOUNDATION_EXPORT double CRDIVersionNumber;
+
+//! Project version string for CRDI.
+FOUNDATION_EXPORT const unsigned char CRDIVersionString[];
+
+
+#import <CRDI/CRDIConfiguration.h>
+#import <CRDI/CRDIContainer.h>
+#import <CRDI/CRDIInjector.h>
